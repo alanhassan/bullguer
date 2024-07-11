9929254069d47747a059599c8da17828104a1157
alanhassan <alanwhassan@hotmail.com>
2024-07-10 23:03:51 -0300

    first commit

--- dashboards.py
+++ dashboards.py
@@ -84,7 +84,7 @@ container3 = st.container()
 # Primeiro container com o primeiro gráfico
 with container1:
     unidade_total = df_filtered.groupby(['Unidade'])[['Nota']].mean().reset_index()
-    fig_nota_unidade = px.bar(unidade_total, y="Nota", x="Unidade", title="Unidade", height=500)
+    fig_nota_unidade = px.bar(unidade_total, y="Nota", x="Unidade", height=500)
     fig_nota_unidade.update_traces(marker_color=color_bars(unidade_total['Nota']), texttemplate='%{y:.2f}', textposition='outside') 
     fig_nota_unidade.update_layout(
         title_x=0.5, 
@@ -99,7 +99,7 @@ with container1:
 # Segundo container com o segundo gráfico
 with container2:
     categoria_total = df_filtered.groupby(['Categoria'])[['Nota']].mean().reset_index()
-    fig_nota_categoria = px.bar(categoria_total, y="Nota", x="Categoria", title="Categoria", height=500)
+    fig_nota_categoria = px.bar(categoria_total, y="Nota", x="Categoria", height=500)
     fig_nota_categoria.update_traces(marker_color=color_bars(categoria_total['Nota']), texttemplate='%{y:.2f}', textposition='outside') 
     fig_nota_categoria.update_layout(
         title_x=0.5, 
@@ -114,7 +114,7 @@ with container2:
 # Terceiro container com o terceiro gráfico
 with container3:
     integrante_total = df_filtered.groupby(['Integrante'])[['Nota']].mean().reset_index()
-    fig_nota_integrante = px.bar(integrante_total, y="Nota", x="Integrante", title="Integrante", height=500)
+    fig_nota_integrante = px.bar(integrante_total, y="Nota", x="Integrante", height=500)
     fig_nota_integrante.update_traces(marker_color=color_bars(integrante_total['Nota']), texttemplate='%{y:.2f}', textposition='outside') 
     fig_nota_integrante.update_layout(
         title_x=0.5, 
